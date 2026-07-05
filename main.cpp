60105b4d480ccd5832cc4cf9cebc56a593c5d605
atrocity1 <neow574@gmail.com>
2026-07-05 21:28:35 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "crow.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace crow;
+
+int main(){
+    SimpleApp app;
+    CROW_ROUTE(app, "/download").methods(HTTPMethod::POST, HTTPMethod::GET)([](const crow::request& req){
+        crow::query_string params("?" + req.body);
+        if (!params.get("url"))
+        {
+            return response(400, "No Url");
+        }
+        std::string youtube_url = params.get("url");
+        std::cout << "Downloading: " << youtube_url << "\n";
+        std::string cmd = "yt-dlp -f mp4 -o 'public/video.mp4' \"" + youtube_url + "\"";
+        std::system(cmd.c_str());
+        response res;
+        res.set_static_file_info("public/video.mp4");
+        res.set_header("Content-Type", "video/mp4");
+        res.set_header("Content-Disposition", "attachment; filename=\"video.mp4\"");
+        return res;
+    });
+    app.port(8080).multithreaded().run();
+    return 0;
+}
