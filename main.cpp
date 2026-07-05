0a47777905f762609556cfc7e0295ecd6fda74c1
atrocity1 <neow574@gmail.com>
2026-07-05 22:35:33 +0100

    Implemented twiter downloader

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <filesystem>
 
 using namespace crow;
+using namespace std;
+namespace fs = std::filesystem;
 
 int main(){
     SimpleApp app;
@@ -13,21 +16,32 @@ int main(){
         {
             return response(400, "No Url");
         }
-        std::string youtube_url = params.get("url");
-        std::cout << "Downloading: " << youtube_url << "\n";
-        std::string cmd = "yt-dlp -q --no-warnings -f \"b[ext=mp4]\" -o 'public/video.mp4' \"" + youtube_url + "\"";
-        int terminal = std::system(cmd.c_str());
+        std::string filepath = "public/video.mp4";
+        if (fs::exists(filepath))
+        {
+            fs::remove(filepath);
+        }
+        string youtube_url = params.get("url");
+        cout << "Downloading: " << youtube_url << "\n";
+        string cmd = "yt-dlp -q --no-warnings -f \"b[ext=mp4]\" -o 'public/video.mp4' \"" + youtube_url + "\"";
+        int terminal = system(cmd.c_str());
         if (terminal)
         {
-            std::cout << "ERROR: video not founded";
+            cout << "ERROR: video not founded";
             return response(500, "Error to download video");
         }
-        
-        
         response res;
         res.set_static_file_info("public/video.mp4");
         res.set_header("Content-Type", "video/mp4");
         res.set_header("Content-Disposition", "attachment; filename=\"video.mp4\"");
+        std::thread([filepath]() {
+            // so pra nao crashar
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            if (fs::exists(filepath)) {
+                fs::remove(filepath);
+                std::cout << "Arquivo temporario " << filepath << " apagado com sucesso!\n";
+            }
+        }).detach();
         return res;
     });
     app.port(8080).multithreaded().run();
--- twitter.cpp
+++ twitter.cpp
@@ -1,13 +1,47 @@
 #include "crow.h"
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <filesystem>
 
 using namespace std;
 using namespace crow;
+namespace fs = std::filesystem;
 
 int main(){
     SimpleApp twitter;
     CROW_ROUTE(twitter, "/download/twitter").methods(HTTPMethod::POST)([](const request& req){
         query_string params("?" + req.body);
+        if (!params.get("url"))
+        {
+            return response(400, "URL not founded");
+        }
+        std::string filepath = "public/video.mp4";
+        if (fs::exists(filepath))
+        {
+            fs::remove(filepath);
+        }
+        string url = params.get("url");
+        cout << "Downloading..." << "\n";
+        string cmd = "yt-dlp -q --no-warnings -f \"b[ext=mp4]\" -o 'public/video.mp4' \"" + url + "\"";
+        int terminal = system(cmd.c_str()); // se vc for fazer um const char * url[] nao precisa fazer esse .c_str pode colocar so cmd
+        if (terminal != 0)
+        {
+            cout << "ERROR: video not founded";
+            return response(500, "Error to download video");
+        }
+        response res;
+        res.set_static_file_info("public/video.mp4"); 
+        res.set_header("Content-Type", "video/mp4");
+        res.set_header("Content-Disposition", "attachment; filename=\"video.mp4\"");
+        std::thread([filepath]() {
+            // so pra nao crashar
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            if (fs::exists(filepath)) {
+                fs::remove(filepath);
+                std::cout << "Arquivo temporario " << filepath << " apagado com sucesso!\n";
+            }
+        }).detach();
+        return res;
     });
 }
